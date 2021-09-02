e9057d4c6ca6735b0c2dbbba8cc9fb3cda2f0ebe
Alexis <alexis.prunier@securitymadein.lu>
2021-09-02 17:08:17 +0200

    Add Articles on company page

--- src/component/PageCompany.jsx
+++ src/component/PageCompany.jsx
@@ -8,27 +8,102 @@ import Loading from "./box/Loading.jsx";
 import TreeTaxonomy from "./chart/TreeTaxonomy.jsx";
 import NoImage from "./box/NoImage.jsx";
 import { getApiURL, getMainAppURL } from "../utils/env.jsx";
+import { dictToURI } from "../utils/url.jsx";
+import DynamicTable from "./table/DynamicTable.jsx";
+import Tab from "./tab/Tab.jsx";
+import Article from "./item/Article.jsx";
+import Event from "./item/Event.jsx";
+import JobOffer from "./item/JobOffer.jsx";
 
 export default class PageCompany extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.getCompanyContent = this.getCompanyContent.bind(this);
+		this.getCompanyNews = this.getCompanyNews.bind(this);
+		this.getCompanyEvents = this.getCompanyEvents.bind(this);
+		this.getCompanyJobOffers = this.getCompanyJobOffers.bind(this);
 		this.getIndustryVertical = this.getIndustryVertical.bind(this);
 
 		this.state = {
 			company: null,
+			news: null,
+			events: null,
+			jobOffers: null,
 		};
 	}
 
 	componentDidMount() {
 		this.getCompanyContent();
+		this.getCompanyNews();
+		this.getCompanyEvents();
+		this.getCompanyJobOffers();
 	}
 
 	getCompanyContent() {
 		getRequest.call(this, "public/get_public_company/" + this.props.match.params.id, (data) => {
 			this.setState({
 				company: data,
+			});
+		}, (response) => {
+			nm.warning(response.statusText);
+		}, (error) => {
+			nm.error(error.message);
+		});
+	}
+
+	getCompanyNews(page) {
+		const params = {
+			type: "NEWS",
+			companies: this.props.match.params.id,
+			page: page || 1,
+			per_page: 3,
+		};
+
+		getRequest.call(this, "public/get_public_articles?"
+			+ dictToURI(params), (data) => {
+			this.setState({
+				news: data,
+			});
+		}, (response) => {
+			nm.warning(response.statusText);
+		}, (error) => {
+			nm.error(error.message);
+		});
+	}
+
+	getCompanyEvents(page) {
+		const params = {
+			type: "EVENT",
+			companies: this.props.match.params.id,
+			page: page || 1,
+			per_page: 3,
+		};
+
+		getRequest.call(this, "public/get_public_articles?"
+			+ dictToURI(params), (data) => {
+			this.setState({
+				events: data,
+			});
+		}, (response) => {
+			nm.warning(response.statusText);
+		}, (error) => {
+			nm.error(error.message);
+		});
+	}
+
+	getCompanyJobOffers(page) {
+		const params = {
+			type: "JOB OFFER",
+			companies: this.props.match.params.id,
+			page: page || 1,
+			per_page: 3,
+		};
+
+		getRequest.call(this, "public/get_public_articles?"
+			+ dictToURI(params), (data) => {
+			this.setState({
+				jobOffers: data,
 			});
 		}, (response) => {
 			nm.warning(response.statusText);
@@ -182,6 +257,79 @@ export default class PageCompany extends React.Component {
 						height={400}
 					/>
 				}
+
+				<div className="row">
+					<div className="col-md-12">
+						<h3>Articles</h3>
+					</div>
+
+					<div className="col-md-12">
+						<Tab
+							keys={["NEWS", "EVENTS", "JOB OFFERS"]}
+							labels={[
+								`News (${this.state.news ? this.state.news.pagination.total : "?"})`,
+								`Events (${this.state.events ? this.state.events.pagination.total : "?"})`,
+								`Job offers (${this.state.jobOffers ? this.state.jobOffers.pagination.total : "?"})`,
+							]}
+							content={[
+								this.state.news !== null
+									? <div className="col-md-12">
+										<DynamicTable
+											items={this.state.news.items}
+											pagination={this.state.news.pagination}
+											changePage={(page) => this.getCompanyNews(page)}
+											buildElement={(a) => <div className="col-md-4">
+												<Article
+													info={a}
+													analytics={this.props.analytics}
+												/>
+											</div>
+											}
+										/>
+									</div>
+									: <Loading
+										height={150}
+									/>,
+								this.state.events !== null
+									? <div className="col-md-12">
+										<DynamicTable
+											items={this.state.events.items}
+											pagination={this.state.events.pagination}
+											changePage={(page) => this.getCompanyEvents(page)}
+											buildElement={(a) => <div className="col-md-4">
+												<Event
+													info={a}
+													analytics={this.props.analytics}
+												/>
+											</div>
+											}
+										/>
+									</div>
+									: <Loading
+										height={150}
+									/>,
+								this.state.jobOffers !== null
+									? <div className="col-md-12">
+										<DynamicTable
+											items={this.state.jobOffers.items}
+											pagination={this.state.jobOffers.pagination}
+											changePage={(page) => this.getCompanyJobOffers(page)}
+											buildElement={(a) => <div className="col-md-12">
+												<JobOffer
+													info={a}
+													analytics={this.props.analytics}
+												/>
+											</div>
+											}
+										/>
+									</div>
+									: <Loading
+										height={150}
+									/>,
+							]}
+						/>
+					</div>
+				</div>
 
 				{this.state.company !== null
 					&& this.props.taxonomy !== null
---
+++ src/component/item/Article.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import "./Article.css";
+import NoImage from "../box/NoImage.jsx";
+import { getApiURL, getMainAppURL } from "../../utils/env.jsx";
+import { dateToString } from "../../utils/date.jsx";
+
+export default class Article extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		};
+	}
+
+	getBoxContent() {
+		return <div className="Article card">
+			<div className="card-img-wrapper">
+				{this.props.info.image !== null && this.props.info.image !== undefined
+					? <img
+						className="card-img-top"
+						src={getApiURL() + "public/get_image/" + this.props.info.image}
+						alt="Card image cap"/>
+					: <NoImage
+						height={200}
+					/>
+				}
+				{this.props.hidePublicationDate === undefined
+					|| this.props.hidePublicationDate === false
+					? <div className="card-date">
+						{dateToString(this.props.info.publication_date, "DD MMM YYYY")}
+					</div>
+					: ""}
+			</div>
+			<div className="card-body">
+				<h5 className="card-title">{this.props.info.title}</h5>
+
+				<button
+					className={"blue-background"}
+				>
+					Know more
+				</button>
+			</div>
+		</div>;
+	}
+
+	render() {
+		return this.props.info.link !== null
+			&& this.props.info.link !== undefined
+			&& this.props.info.link.length > 0
+			? <a
+				href={this.props.info.link}
+				target={"_blank"}
+				rel="noreferrer"
+				className="Article-link">
+				{this.getBoxContent()}
+			</a>
+			: <a
+				href={getMainAppURL() + "/news/" + this.props.info.handle}
+				className="Article-link">
+				{this.getBoxContent()}
+			</a>;
+	}
+}
