a882c78bb7d6e10fb35eae987855221b6a43c320
Alexis <alexis.prunier@securitymadein.lu>
2021-10-13 14:01:08 +0200

    Add boxes on private sector page

--- src/component/PagePrivateSector.jsx
+++ src/component/PagePrivateSector.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./PagePrivateSector.css";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import { Link } from "react-router-dom";
+import Popup from "reactjs-popup";
 import { NotificationManager as nm } from "react-notifications";
 import { getRequest } from "../utils/request.jsx";
 import Loading from "./box/Loading.jsx";
@@ -13,14 +14,15 @@ import BarWorkforceRange from "./chart/BarWorkforceRange.jsx";
 import BarActorAge from "./chart/BarActorAge.jsx";
 import { getUrlParameter, dictToURI } from "../utils/url.jsx";
 import VennActorDistribution from "./chart/VennActorDistribution.jsx";
-import ButtonRegister from "./form/ButtonRegister.jsx";
-import { getMainAppURL } from "../utils/env.jsx";
+import { getMainAppURL, getPrivateSpaceURL } from "../utils/env.jsx";
 
 export default class PagePrivateSector extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.getCompanies = this.getCompanies.bind(this);
+		this.getPCDoctors = this.getPCDoctors.bind(this);
+		this.getCoreStartups = this.getCoreStartups.bind(this);
 		this.getAnalytics = this.getAnalytics.bind(this);
 		this.getTotalEmployees = this.getTotalEmployees.bind(this);
 		this.onSearch = this.onSearch.bind(this);
@@ -28,6 +30,8 @@ export default class PagePrivateSector extends React.Component {
 
 		this.state = {
 			actors: null,
+			pcDoctors: null,
+			coreStartups: null,
 			analytics: null,
 			filters: {
 				name: getUrlParameter("name"),
@@ -72,6 +76,52 @@ export default class PagePrivateSector extends React.Component {
 		}, (error) => {
 			nm.error(error.message);
 		});
+	}
+
+	getPCDoctors() {
+		if (this.state.analytics) {
+			this.setState({
+				pcDoctors: null,
+			}, () => {
+				const individualValues = this.state.analytics.taxonomy_values
+					.filter((v) => v.category === "ENTITY TARGET" && v.name === "INDIVIDUAL");
+
+				if (individualValues.length === 1) {
+					getRequest.call(this, "public/get_public_companies?"
+						+ "ecosystem_role=ACTOR&entity_type=PRIVATE%20SECTOR"
+						+ "&taxonomy_values=" + individualValues[0].id, (data) => {
+						this.setState({
+							pcDoctors: data,
+						});
+					}, (response) => {
+						nm.warning(response.statusText);
+					}, (error) => {
+						nm.error(error.message);
+					});
+				}
+			});
+		}
+	}
+
+	getCoreStartups() {
+		if (this.state.analytics) {
+			this.setState({
+				coreStartups: null,
+			}, () => {
+				getRequest.call(this, "public/get_public_companies?"
+					+ "ecosystem_role=ACTOR&entity_type=PRIVATE%20SECTOR"
+					+ "&corebusiness_only=true&startup_only=true",
+				(data) => {
+					this.setState({
+						coreStartups: data,
+					});
+				}, (response) => {
+					nm.warning(response.statusText);
+				}, (error) => {
+					nm.error(error.message);
+				});
+			});
+		}
 	}
 
 	getAnalytics() {
@@ -129,12 +179,6 @@ export default class PagePrivateSector extends React.Component {
 					</div>
 				</div>
 
-				<div className="row">
-					<div className="col-md-12 right-buttons">
-						<ButtonRegister/>
-					</div>
-				</div>
-
 				<PrivateSectorSearch
 					analytics={this.state.analytics}
 					filters={this.state.filters}
@@ -143,7 +187,10 @@ export default class PagePrivateSector extends React.Component {
 
 				<div className="row">
 					<div className="col-md-12">
-						<h1>{this.state.actors !== null ? this.state.actors.length + " " : ""}companies</h1>
+						<h3>{this.state.actors !== null
+							? this.state.actors.length + " companies found"
+							: "Loading"}
+						</h3>
 					</div>
 				</div>
 
@@ -177,6 +224,147 @@ export default class PagePrivateSector extends React.Component {
 						</div>
 					</div>
 				}
+
+				<div className="row row-spaced">
+					<div className="col-md-12">
+						<h1>CYBERSECURITY Luxembourg selection</h1>
+					</div>
+				</div>
+
+				<div className="row row-spaced">
+					<div className="col-md-4">
+						<Popup
+							className={"Popup-full-size"}
+							trigger={
+								<div className="shadow-section blue-shadow-section centered-shadow-section">
+									{/* eslint-disable no-script-url */}
+									<a>
+										<div>
+											<h3>PC Doctors</h3>
+											<i className="fas fa-user-md"/>
+											<div className="PageHome-newsletter-content-desc">
+												See the list of companies providing services in order
+												to support the individuals
+											</div>
+										</div>
+									</a>
+								</div>
+							}
+							onOpen={this.getPCDoctors}
+							modal
+						>
+							{(close) => (
+								<div className={"row PageHome-how-to-use-content"}>
+									<div className={"col-md-12"}>
+										{// eslint-disable-next-line
+										}<h2>PC Doctors</h2>
+										<div className="top-right-buttons">
+											<button
+												className={"red-background"}
+												onClick={close}>
+												<i className="fas fa-times"/>
+											</button>
+										</div>
+									</div>
+
+									{this.state.pcDoctors
+										? <div className={"col-md-12"}>
+											<div className={"row"}>
+												{this.state.pcDoctors.map((c) => <div
+													key={c.id}
+													className={"col-md-6"}>
+													<Company
+														info={c}
+													/>
+												</div>)}
+											</div>
+										</div>
+										: <div className={"col-md-12"}>
+											<Loading
+												height={300}
+											/>
+										</div>
+									}
+								</div>
+							)}
+						</Popup>
+					</div>
+					<div className="col-md-4">
+						<Popup
+							className={"Popup-full-size"}
+							trigger={
+								<div className="shadow-section blue-shadow-section centered-shadow-section">
+									{/* eslint-disable no-script-url */}
+									<a>
+										<div>
+											<h3>Pure Cyber Startups</h3>
+											<i className="fas fa-rocket"/>
+											<div className="PageHome-newsletter-content-desc">
+												Have a look on the startups with cybersecurity
+												as a core business
+											</div>
+										</div>
+									</a>
+								</div>
+							}
+							onOpen={this.getCoreStartups}
+							modal
+						>
+							{(close) => (
+								<div className={"row PageHome-how-to-use-content"}>
+									<div className={"col-md-12"}>
+										{// eslint-disable-next-line
+										}<h2>Pure Cyber Startups</h2>
+										<div className="top-right-buttons">
+											<button
+												className={"red-background"}
+												onClick={close}>
+												<i className="fas fa-times"/>
+											</button>
+										</div>
+									</div>
+
+									{this.state.coreStartups
+										? <div className={"col-md-12"}>
+											<div className={"row"}>
+												{this.state.coreStartups.map((c) => <div
+													key={c.id}
+													className={"col-md-6"}>
+													<Company
+														info={c}
+													/>
+												</div>)}
+											</div>
+										</div>
+										: <div className={"col-md-12"}>
+											<Loading
+												height={300}
+											/>
+										</div>
+									}
+								</div>
+							)}
+						</Popup>
+					</div>
+					<div className="col-md-4">
+						<div className="shadow-section red-shadow-section centered-shadow-section">
+							{/* eslint-disable no-script-url */}
+							<a
+								href={getPrivateSpaceURL()}
+								target="_blank"
+								rel="noreferrer">
+								<div>
+									<h3>My CYBERLUX</h3>
+									<i className="fas fa-plug"/>
+									<div className="PageHome-newsletter-content-desc">
+										Register or update your company and show the best
+										of your organisation
+									</div>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
 
 				<div className="row row-spaced">
 					<div className="col-md-12">
