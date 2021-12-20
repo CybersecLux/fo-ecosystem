935b7ec69576e1affb1f96cc01e5ad0edd43e3b8
Alexis <alexis.prunier@securitymadein.lu>
2021-12-20 16:29:10 +0100

    Remove specific env endpoint

--- src/utils/env.jsx
+++ src/utils/env.jsx
@@ -9,9 +9,6 @@ export function getPrivateSpaceURL() {
 	if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
 		return "http://localhost:3001/";
 	}
-	if (window.location.hostname.includes("test")) {
-		return "https://test-my.cy.lu/";
-	}
 	return "https://my." + window.location.hostname.replace("www.", "").split(".").slice(1).join(".") + "/";
 }
 
